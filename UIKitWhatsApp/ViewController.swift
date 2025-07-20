c2c1f87fb72994ffd4e49ab8a7fe473d2fe07cc2
Siam300 <89306483+Siam300@users.noreply.github.com>
2025-07-20 21:23:43 +0600

    Initial Commit

---
+++ UIKitWhatsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitWhatsApp
+//
+//  Created by Auto on 7/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
